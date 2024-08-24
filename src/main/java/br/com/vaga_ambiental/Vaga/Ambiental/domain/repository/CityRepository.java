6fe46e20d5f2c1e9f7db74976ab490d0e2f5f34f
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-08-24 19:11:52 -0300

    feat: add repositories

---
+++ src/main/java/br/com/vaga_ambiental/Vaga/Ambiental/domain/repository/CityRepository.java
@@ -0,0 +1,11 @@
+package br.com.vaga_ambiental.Vaga.Ambiental.domain.repository;
+
+import br.com.vaga_ambiental.Vaga.Ambiental.domain.model.Entity.CityEntity;
+import br.com.vaga_ambiental.Vaga.Ambiental.domain.model.Entity.StateEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CityRepository extends JpaRepository<CityEntity, Long> {
+    Optional<CityEntity> findByNameAndState(String cityName, StateEntity state);
+}
---
+++ src/main/java/br/com/vaga_ambiental/Vaga/Ambiental/domain/repository/HolidayRepository.java
@@ -0,0 +1,7 @@
+package br.com.vaga_ambiental.Vaga.Ambiental.domain.repository;
+
+import br.com.vaga_ambiental.Vaga.Ambiental.domain.model.Entity.HolidayEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HolidayRepository extends JpaRepository<HolidayEntity, Long> {
+}
