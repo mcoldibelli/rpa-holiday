d6b4f31b6be669dd98c7aaa5a819b5b8906abf38
Marcelo A Coldibelli <marcelo.coldibelli@gmail.com>
2024-08-24 15:46:58 -0300

    refactor: simplify model

--- src/main/java/br/com/vaga_ambiental/Vaga/Ambiental/domain/model/Holiday.java
+++ src/main/java/br/com/vaga_ambiental/Vaga/Ambiental/domain/model/Holiday.java
@@ -5,28 +5,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Holiday {
     private LocalDate date;
     private String name;
     private HolidayType type;
-
-    public Holiday(String date, String name, HolidayType type) {
-        this.date = parseDate(date);
-        this.name = name;
-        this.type = type;
-    }
-
-    private LocalDate parseDate(String date) {
-        try {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-            return LocalDate.parse(date, formatter);
-        } catch(DateTimeParseException e) {
-            throw new RuntimeException("Invalid date format: " + date, e);
-        }
-    }
 }
